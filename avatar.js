f71a7d1427f9d819d2fc8163c919ff6b5c3a4f04
EC2 Default User <ec2-user@ip-172-31-78-81.ec2.internal>
2018-10-17 19:56:38 +0000

    new commit

---
+++ avatar.js
@@ -0,0 +1,48 @@
+angular.module('app', [])
+  .controller('mainCtrl', mainCtrl)
+  .directive('avatar', avatarDirective);
+
+function mainCtrl ($scope) {
+
+  $scope.users = [];
+
+  $scope.addNew = function (user) {
+    $scope.users.push({ 
+      name: user.name,
+      email: user.email,
+      avatarUrl: user.url
+    }); /* [1] */
+    
+    user.name = ''; /* [2] */
+    user.email = '';
+    user.url = ''; 
+  };
+}
+
+function avatarDirective () {
+  return {
+    scope: {
+      user: '=' /* [1] */
+    },
+    restrict: 'E', /* [2] */
+    replace: 'true',
+    template: (
+      '<div class="Avatar">' +
+        '<img ng-src="{{user.avatarUrl}}" />' +
+        '<h4>{{user.name}}</h4>' +
+        '<h4>{{user.email}}</h4>' + 
+      '</div>'
+    ), /* [3] */
+    link: link
+  };
+  
+  function link (scope) { /* [4] */
+    if (!scope.user.avatarUrl) {
+      scope.user.avatarUrl = 'https://www.drupal.org/files/issues/default-avatar.png';
+    }
+    if(!scope.user.email) {
+      scope.user.email = "No email given.";
+    }
+  }
+
+}
