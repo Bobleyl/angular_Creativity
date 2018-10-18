247689c0f8720e4fb90d237707a68cdb98de7023
tchacking <tchbook@gmail.com>
2018-10-18 20:03:08 +0000

    Say something

--- avatar.js
+++ avatar.js
@@ -1,18 +1,30 @@
-var app = angular.module('myApp', []); 
-app.controller('imageCtrl', function($scope) {
-    $scope.imageList = [];
-    $scope.imageList.push({path:'/test.jpg', done:false})
+var randNum = Math.floor(Math.random() * 5) + 1;
 
-    $scope.imageAdd = function() {
-        $scope.imageList.push({path:$scope.imageInput, done:false});
-        $scope.imageInput = "";
+var app = angular.module('myApp', [])
+app.controller('myCtrl', function($scope, $http) {
+
+    $scope.gifSearchForUrl = function() {
+        $scope.newGifSearch = $scope.gifSearch.replace(/ /g, "");
     };
 
-    $scope.remove = function() {
-        var oldList = $scope.imageList;
-        $scope.imageList = [];
-        angular.forEach(oldList, function(x) {
-            if (!x.done) $scope.imageList.push(x);
+    $scope.showGifs = function() {
+        $scope.gifList = [];
+        $scope.url = "http://api.giphy.com/v1/gifs/search?q=";
+        $scope.gifSearch = $scope.gifInput;
+        $scope.gifInput = "";
+        $scope.gifSearchForUrl();
+
+        $scope.url = $scope.url + $scope.newGifSearch + "&api_key=FGlxsqMvwvI2dcq5inIXZFaf3E6ivyfK&limit=5";
+
+        $http.get($scope.url).then(function(response) {
+
+            $scope.gifList.push(response['data']['data'][randNum]['images']['original']['mp4']);
+            /*  var tmpgifList = [];
+              for(var i = 0; i < response.length; i++) {
+                  tmpgifList.push(response['data'][i]['embed_url']);
+                  console.log(tmpgifList);
+              };
+              console.log(tmpgifList);*/
         });
     };
 });
